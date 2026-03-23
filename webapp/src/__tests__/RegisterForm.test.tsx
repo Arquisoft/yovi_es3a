6d237b80292c5255f9de0bfd76a2f6d5679c51aa
uo288595 <uo288595@uniovi.es>
2026-03-23 09:51:21 +0100

    Comprobación de fallos

--- webapp/src/__tests__/RegisterForm.test.tsx
+++ webapp/src/__tests__/RegisterForm.test.tsx
@@ -1,11 +1,36 @@
 import { describe, expect, it } from 'vitest' 
+import RegisterForm from '../RegisterForm'
 import '@testing-library/jest-dom'
+import { render, screen, fireEvent } from '@testing-library/react';
 
 
 describe('RegisterForm', () =>
 {
     it('renders without errors', () =>
     {
-        expect(true).toBe(true)
+        <RegisterForm />
+
+        // Mira si los labels están en el documento y tienen el texto correcto
+        let label = document.querySelector('label[for="username"]');
+        expect(label).toBeInTheDocument()
+        expect(label).toHaveTextContent('Whats your name?')
+
+        label = document.querySelector('label[for="password"]');
+        expect(label).toBeInTheDocument()
+        expect(label).toHaveTextContent('Whats your password?')
+    })
+
+    it ('register new user', () => {
+        <RegisterForm />
+
+        // Rellena los campos username y password
+        const usernameInput = screen.getByLabelText('Whats your name?');
+        expect(usernameInput).toBeInTheDocument();
+
+        const passwordInput = screen.getByLabelText('Whats your password?');
+        expect(passwordInput).toBeInTheDocument();
+
+        fireEvent.change(passwordInput, { target: { value: 'test-username' } });
+        fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
     })
 })
