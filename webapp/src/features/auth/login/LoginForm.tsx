47d3c6880f6f688cecaf98b4b4411e8e5dfd810b
pispidu <aresgf03@gmail.com>
2026-03-23 08:59:27 +0100

    Cambios menores CSS

--- webapp/src/features/auth/login/LoginForm.tsx
+++ webapp/src/features/auth/login/LoginForm.tsx
@@ -61,15 +61,15 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSuccess }) =>
     <form onSubmit={handleSubmit} className="w-100">
       
       <div className="text-center mb-4">
-        <h2 className='purple-fg fw-bold display-5'>Inicio de sesión</h2>
+        <h2 className='dark-purple-fg fw-bold display-5'>Inicio de sesión</h2>
       </div>
       
       <div className="mb-3">
-        <label htmlFor="username" className="form-label text-white">
+        <label htmlFor="username" className="form-label fw-bold">
           Nombre de usuario:
         </label>
         <div className="input-group">
-          <span className="input-group-text bg-transparent border-end-0 text-white">
+          <span className="input-group-text bg-transparent border-end-0">
             <i className="bi bi-key-fill"></i>
           </span>
           <input
@@ -77,17 +77,17 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSuccess }) =>
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="form-control bg-transparent border-start-0 text-white"
+            className="form-control bg-transparent border-start-0"
           />
         </div>
       </div>
 
       <div className="mb-3">
-        <label htmlFor="password" className="form-label text-white">
+        <label htmlFor="password" className="form-label fw-bold">
           Contraseña:
         </label>
         <div className='input-group'>
-          <span className="input-group-text bg-transparent border-end-0 text-white">
+          <span className="input-group-text bg-transparent border-end-0">
             <i className="bi bi-person-fill"></i>
           </span>
           <input
@@ -95,7 +95,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSuccess }) =>
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="form-control bg-transparent border-start-0 text-white"
+            className="form-control bg-transparent border-start-0"
           />
         </div>
       </div>
@@ -106,8 +106,8 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSuccess }) =>
 
       <div className="text-center mt-4">
         <p className="d-flex align-items-center justify-content-center">
-          <span className='text-white me-1'> ¿No tienes cuenta? </span>
-          <NavLink to="/register" className="purple-fg fw-bold text-decoration-none ms-2">
+          <span className='me-1'> ¿Ya estás registrado? </span>
+          <NavLink to="/register" className="dark-purple-fg fw-bold text-decoration-none ms-2">
             Crear cuenta
           </NavLink>
         </p>
