d96143119a52977fbf448124ae511e0de12d9089
Ben James Coleman Kheyyali <uo284238@uniovi.es>
2026-04-28 23:26:36 +0200

    Corrección encipción de passwords

--- users/user-service.js
+++ users/user-service.js
@@ -94,7 +94,8 @@ const checkPassword = async (username, password) =>
     if (!user) return false;
 
     // Use bcrypt.compare to verify the plain password against the stored hash.
-    return await bcrypt.compare(password, user.password);
+    const hashed = await bcrypt.hash(password, 10);
+    return await bcrypt.compare(hashed, user.password);
 }
 
 module.exports = {
